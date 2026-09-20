be554afdb207757ad441a992e117b22f80d03c24
Tove Kaleba <tove@berkeley.edu>
2026-09-20 16:28:27 -0700

    done with hw 2

---
+++ homework2/homework2.py
@@ -0,0 +1,63 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash? Git is the main software, GitHub is a website to host Git files, 
+# and Git Bash is a command
+
+# 2) What’s the difference between the terminal and the command line? The terminal is the interface to access the command line 
+# where you do the actual coding. 
+
+# 3) How does Windows PowerShell differ from Git Bash? PowerShell is owned by microsoft and allows for scripting, 
+# and GitBash is owned by someobody else and is in a different format.
+
+# 4) What’s the difference between Anaconda, conda, and Python? Anaconda bundles conda and python, python is a programming 
+# language, and conda is a tool to manage packages and enviroments.
+
+# 5) What is VS Code? VS code is a software that allows for code editing.
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab? Jupyter notebook is a single document interface, 
+# Jupyter lab is a modular workspace for multi-file projects.
+
+# 7) What does ~/ mean? current users home directory
+
+# 8) What’s the difference between an absolute path and a relative path? absolute path gives the full address from the root, while the root gives the address 
+# based on current folder.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2". 
+# Absolute: desktop/python_decal/course_assignments/homework2/homework2.py course_assignments/homework2/homework2.py I noticed there is an inconsistency in the 
+# homework directions where we created the course assignments folder in our python directory but the rest of the directions refer to the folder being in the 
+# "your name" directory.
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"? cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!) delete the directory and all its contents
+
+# 12) What do the following commands do?
+# git add prepare 
+# git commit save locally
+# git push upload/save/transfer
+
+# 13) What's the difference between "git add ." and "git add <file>"? git add . adds everything in 
+# current directory while git add file name adds only that file
+
+# 14) What do "git status" and "git log -1" do? git status shows current status of every file in directory, git log -1 
+# shows only the most recent commit.
+
+# 15) What’s the difference between cloning a repository and pulling from it? cloning it makes two of the same, 
+# pulling from it makes copies of the contents into a new location
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it? 
+# Honestly when I setup git for the first time and I kept committing the wrong repository because I either didn't read the 
+# directions correctly (I didn't have a folder with my name) or the directions were inconsistent
+
+# 17) What’s a question you still have? What’s something you’re confused about? Im curious about why we use git.
+
+# 18) Tell me a fun fact! Whale babies play games with eachother.
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print(20 % 7) # remainder of division
